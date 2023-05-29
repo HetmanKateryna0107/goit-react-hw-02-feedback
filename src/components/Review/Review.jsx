33444848ffed2b7b83907e61f8b8b0506da09fae
HetmanKateryna0107 <125737708+HetmanKateryna0107@users.noreply.github.com>
2023-05-29 15:24:11 +0300

    caunter

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -3,31 +3,51 @@ import { Component } from 'react';
 import css from './Review.module.css';
 
 export class Review extends Component {
- state={counter:this.props.defaultValue ?? 0 }
+ state={
+    
+    counterGood:this.props.defaultValue ?? 0 ,
+    counterNeutral :this.props.defaultValue ?? 0 ,
+    counterBad:this.props.defaultValue ?? 0 }
 
- handlePlus = () => {
-    this.setState((prevState)=>({counter: prevState.counter + 1}))
+ handleGood = () => {
+    this.setState((prevState)=>({counterGood: prevState.counterGood + 1}))
+ }
+ handleNeutral = () => {
+    this.setState((prevState)=>({counterNeutral: prevState.counterNeutral + 1}))
+ }
+ handleBad = () => {
+    this.setState((prevState)=>({counterBad: prevState.counterBad + 1}))
  }
  handleUpdate =(event) =>{
 const {name} = event.target
 switch (name){
     case 'good':
+        this.setState((prevState)=>({counterGood: prevState.counterGood + 1}))
+break;
+case 'neutral':
+    this.setState((prevState)=>({counterNeutral: prevState.counterNeutral + 1}))
+break;
+case 'bad':
+    this.setState((prevState)=>({counterBad: prevState.counterBad + 1}))
+    break;
+default:
+    throw new Error ("There is no feedback")
 }
  }
 
     render(){
-        const {counter} =this.state;
+        const {counterGood, counterNeutral, counterBad} =this.state;
     return (
         <div >
           <h2>Pleace leave feedback</h2>
-          <button name='good' onClick={this.handleUpdate} className={css.btn}>Good</button>
-          <button name='neutral'onClick={this.handleUpdate} className={css.btn}>Neutral</button>
-          <button name='bad' onClick={this.handleUpdate} className={css.btn}>Bad</button>
+          <button name='good' onClick={this.handleGood} className={css.btn}>Good</button>
+          <button name='neutral'onClick={this.handleNeutral} className={css.btn}>Neutral</button>
+          <button name='bad' onClick={this.handleBad} className={css.btn}>Bad</button>
           <h2>Statistics</h2>
-          <ul>
-            <li>Good: {counter}</li>
-            <li>Neutral: {counter}</li>
-            <li>Bad: {counter}</li>
+          <ul className={css.list}>
+            <li>Good: {counterGood}</li>
+            <li>Neutral: {counterNeutral}</li>
+            <li>Bad: {counterBad}</li>
           </ul>
         </div>
       );
